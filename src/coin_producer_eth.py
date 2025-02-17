10a3660fd17a26462ff5d122d17aacccd0aaf1cb
Robin Sundman Nilsson <sundmanrobin@outlook.com>
2025-02-17 11:40:41 +0100

    updated producer for ETH and SOL

--- src/coin_producer_eth.py
+++ src/coin_producer_eth.py
@@ -3,30 +3,24 @@ from connect_api import get_latest_coin_data, get_topic
 import time
 
 def main():
-    # creating Quixstreams application and defining Kafka topic
     app = Application(broker_address="localhost:9092", consumer_group="coin_group")
     eth_topic = get_topic("ETH")
 
-    # using producer to send cryptodata to Kafka
     with app.get_producer() as producer:
         while True:
-            # collecting latest cryptodata for Ethereum
+
             coin_latest = get_latest_coin_data("ETH")
 
-            # serializing Kafka message
             kafka_message = eth_topic.serialize(
                 key=coin_latest["symbol"], value=coin_latest
             )
 
-            # prints log on what's been produced
             print(
-                f"Producer: Skickar {kafka_message.key} - Pris: {coin_latest['quote']['SEK']['price']:.2f} Kr"
+                f"Producer ETH: Skickar {kafka_message.key} - Pris: {coin_latest['quote']['SEK']['price']:.2f} Kr"
             )
 
-            # sends message to Kafka topic
             producer.produce(topic=eth_topic.name, key=kafka_message.key, value=kafka_message.value)
 
-            # waits 30 sec until sending next message
             time.sleep(30)
 
 if __name__ == "__main__":
