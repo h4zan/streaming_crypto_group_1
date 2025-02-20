b7736cef099054f77c2ce0f1b71dee8571c9dcf0
AndresTrujillo1811-bit <andresgtrujillof@gmail.com>
2025-02-20 11:35:42 +0100

    updated radio bottoms

--- src/charts.py
+++ src/charts.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
-
-
+import matplotlib.dates as mdates
+ 
 def line_chart(x, y, **options):
     fig, ax = plt.subplots(1)
-
+ 
     ax.plot(x, y, linewidth=4)
-
+ 
+    ax.xaxis.set_major_locator(mdates.AutoDateLocator())  
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%m-%d %H:%M"))  
+    plt.xticks(rotation=45)  
+ 
     ax.set(**options)
-
+ 
     fig.tight_layout()
     return fig
